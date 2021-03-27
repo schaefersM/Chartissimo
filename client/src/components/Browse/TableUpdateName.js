9ad0bdc8af9d84a686c0bb7ac8a3e25672a096db
Max Schäfers <maximilian.schaefers@gmx.de>
2021-03-27 12:31:49 +0100

    added onFocus trigger to prevent weird behaviour on mobile devices

--- client/src/components/Browse/TableUpdateName.js
+++ client/src/components/Browse/TableUpdateName.js
@@ -1,14 +1,17 @@
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { isMobile } from "react-device-detect";
 import PropTypes from "prop-types";
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useChartDispatch, useChartStore } from "../../stores/chartStore";
 
 
-const TableUpdateName = ({chartId, rowId}) => {
+const GraphName = ({chartId, rowId}) => {
 
     const chartDispatch = useChartDispatch();
     const { charts } = useChartStore();
+
+    const nameRef = useRef(true);
 
     const [errorText, setErrorText] = useState("Insert a new name");
     const [name, setName] = useState('');
@@ -60,29 +63,40 @@ const TableUpdateName = ({chartId, rowId}) => {
         }
     };
 
+    // Mobile only function
+    // Prevent weird behaviour when the mobile url bar toggles.
+    const focusing = () => {
+        setTimeout(() => {
+            nameRef.current.blur();
+            nameRef.current.focus();
+        }, 0);
+    }
+
     return (
         <div>
             <input 
-                className={errorText !== "Insert a new name" ? "error-placeholder mr-3" : "light-placeholder mr-3"}
+                className={errorText !== "Insert a new name" ? "error-placeholder mr-3 align-top" : "light-placeholder mr-3 align-top"}
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
                 placeholder={errorText} 
                 type="text" 
-                value={name} 
+                value={name}
+                ref={nameRef}
+                onClick={isMobile ? focusing : null}
             />
             <FontAwesomeIcon
                 icon={faSave}
                 size="2x"
                 onClick={submitChange}                    
-                className="cursor-pointer align-bottom"
+                className="cursor-pointer"
             />
         </div>
     )
 }
 
-TableUpdateName.propTypes = {
+GraphName.propTypes = {
     chartId: PropTypes.number.isRequired,
     rowId: PropTypes.number.isRequired,
 };
 
-export default TableUpdateName;
+export default GraphName;
