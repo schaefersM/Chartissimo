c158192b266972b6efff4f407ded5b5d197f7aaf
Max Schäfers <maximilian.schaefers@gmx.de>
2021-04-10 12:42:25 +0200

    implement a function to set the fontsize of charts

--- client/src/components/Browse/Options.js
+++ client/src/components/Browse/Options.js
@@ -1,241 +1,192 @@
 import PropTypes from "prop-types";
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from "react";
 import "rc-slider/assets/index.css";
-import Slider from 'rc-slider';
-import SaveConfigModal from './SaveConfigModal';
-import { useAuthStore } from "../../stores/authStore"
+import Slider from "rc-slider";
+import SaveConfigModal from "./SaveConfigModal";
+import { setChartFontsize } from "../../helper";
+import { useAuthStore } from "../../stores/authStore";
 import { useChartStore, useChartDispatch } from "../../stores/chartStore";
 
+const Options = ({ id, options }) => {
+	const { isAuthenticated, user } = useAuthStore();
 
-const Options = ({
-    id, 
-    lineChart, 
-    options
-}) => {
+	const chartDispatch = useChartDispatch();
+	const { charts, customGlobalOptions } = useChartStore();
 
-    const { isAuthenticated, user } = useAuthStore();
+	const charttitleRef = useRef();
 
-    const chartDispatch = useChartDispatch();
-    const {charts, customGlobalOptions } = useChartStore();
+	const [chartTitle, setChartTitle] = useState("");
+	const [fontSize, setFontSize] = useState(
+		charts[id].customOptions.fontSize
+			? charts[id].customOptions.fontSize
+			: customGlobalOptions.fontSize
+	);
+	const [initialFontSize] = useState(
+		charts[id].customOptions.fontSize
+			? charts[id].customOptions.fontSize
+			: customGlobalOptions.fontSize
+	);
+	const [showsaveConfigModal, setShowSaveConfigModal] = useState(false);
 
-    const charttitleRef = useRef();
+	const changeChartTitle = () => {
+		setChartTitle(charttitleRef.current.value);
+		let newChart = charts[id];
+		newChart = {
+			...newChart,
+			options: {
+				...newChart.options,
+				title: {
+					...newChart.options.title,
+					text: charttitleRef.current.value
+						? charttitleRef.current.value
+						: options.title.text,
+				},
+			},
+		};
+		charts.splice(id, 1, newChart);
+		chartDispatch({ type: "updateChart", payload: charts });
+	};
 
-    const [chartTitle, setChartTitle] = useState("");
-    const [fontSize, setFontSize] = useState(
-        isAuthenticated && lineChart.current.props.options.customOptions ? lineChart.current.props.options.customOptions.fontSize : customGlobalOptions.fontSize
-    );
-    const [initialFontSize] = useState(
-        isAuthenticated && lineChart.current.props.options.customOptions ? lineChart.current.props.options.customOptions.fontSize : customGlobalOptions.fontSize
-    );
-    const [showsaveConfigModal, setShowSaveConfigModal] = useState(false);
-    
-    const changeChartTitle = () => {
-        setChartTitle(charttitleRef.current.value);
-        let newChart = charts[id];
-        newChart = {
-            ...newChart,
-            options: {
-                ...newChart.options,
-                title: {
-                    ...newChart.options.title,
-                    text: charttitleRef.current.value
-                        ? charttitleRef.current.value
-                        : options.title.text,
-                },
-            },
-        };
-        charts.splice(id, 1, newChart);
-        chartDispatch({ type: "updateChart", payload: charts });
-    };
+	const handleChange = (e) => {
+		setChartTitle(e.target.value);
+	};
 
-    const handleChange = (e) => {
-        setChartTitle(e.target.value);
-    };
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			e.preventDefault();
+			changeChartTitle();
+			setChartTitle("");
+		}
+	};
 
-    const handleKeyDown = (e) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            changeChartTitle();
-            setChartTitle("");
-        }
-    };
+	const handleRange = (e) => {
+		setFontSize(e);
+		const newChart = setChartFontsize(charts[id], e);
+		charts.splice(id, 1, newChart);
+		chartDispatch({ type: "updateChart", payload: charts });
+	};
 
-    const handleRange = (e) => {
-        setFontSize(e);
+	const handleSave = async (e) => {
+		try {
+			const { user_id } = user;
+			const options = {
+				credentials: "include",
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					config: {
+						fontSize,
+					},
+				}),
+			};
+			const response = await fetch(
+				`http://${process.env.REACT_APP_BACKEND_IP}:5000/api/user/${user_id}/config`,
+				options
+			);
 
-        //computed Values in charts[id] object not possible
+			if (!response.ok) {
+				alert(await response.json());
+				console.log("nope");
+				return null;
+			} else {
+				chartDispatch({
+					type: "setCustomGlobalOptions",
+					payload: { fontSize },
+				});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-        let newChart = charts[id];
-        const newYAxes = charts[id].options.scales.yAxes.map((scale, _) => {
-            return {
-                ticks: {
-                    ...scale.ticks,
-                    fontSize: e,
-                },
-                scaleLabel: {
-                    ...scale.scaleLabel,
-                    fontSize: e,
-                },
-            };
-        });
-        newChart = {
-            ...newChart,
-            options: {
-                ...newChart.options,
-                customOptions: {
-                    ...newChart.options.customOptions,
-                    fontSize: e,
-                },
-                title: {
-                    ...newChart.options.title,
-                    fontSize: e,
-                },
-                legend: {
-                    ...newChart.options.legend,
-                    labels: {
-                        ...newChart.options.legend.labels,
-                        fontSize: e,
-                    },
-                },
-                scales: {
-                    xAxes: [
-                        {
-                            ...newChart.options.scales.xAxes[0],
-                            ticks: {
-                                ...newChart.options.scales.xAxes[0].ticks,
-                                fontSize: e,
-                            },
-                        },
-                    ],
-                    yAxes: newYAxes,
-                },
-            },
-        };
-        charts.splice(id, 1, newChart);
-        chartDispatch({ type: "updateChart", payload: charts });
-    };
+	// const handleSubmit = () => {
+	//     changeChartTitle();
+	//     setChartTitle("");
+	// };
 
-    const handleSave = async (e) => {
-        try {
-            const { user_id } = user;
-            const options = {
-                credentials: "include",
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    config: { 
-                        fontSize
-                    },
-                }),
-            };
-            const response = await fetch(
-                `http://${process.env.REACT_APP_BACKEND_IP}:5000/api/user/${user_id}/config`,
-                options
-            );
-    
-            if (!response.ok) {
-                alert(await response.json());
-                console.log("nope")
-                return null;
-            } else {
-                chartDispatch({
-                    type: "setCustomGlobalOptions",
-                    payload: { fontSize },
-                });
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
-  
-    // const handleSubmit = () => {
-    //     changeChartTitle();
-    //     setChartTitle("");
-    // };
-
-    return (
-        <div className="bg-success mb-2 py-2 px-2 rounded-bottom">
-            <div className="fontsize-container d-flex flex-row flex-nowrap mb-2">
-                <span className="text-light font-weight-bold">Fontsize </span>
-                <Slider
-                    min={10}
-                    max={32}
-                    startPoint={fontSize}
-                    defaultValue={initialFontSize}
-                    className="fontsize-slider py-2 ml-2"
-                    onChange={handleRange}
-                    railStyle={{
-                        backgroundColor: "darkgreen",
-                        height: "10px",
-                        marginRight: "1em"
-                    }}
-                    handleStyle={{
-                        backgroundColor: "#b9b9b9",
-                        borderStyle: "solid",
-                        borderWidth: "2px",
-                        borderColor: "#606060",
-                        padding: "8px",
-                    }}
-                    trackStyle={{
-                        backgroundColor: "#313131",
-                        height: "10px",
-                    }}
-                />
-                {/* ist nötig, weil lineChart.current.chartInstance.update() ein Error wirft, wenn die neue fontSize === aktuelle fontSize ist*/}
-                {fontSize !== initialFontSize && (
-                    <button
-                        onClick={
-                            isAuthenticated
-                                ? handleSave
-                                : () =>
-                                      setShowSaveConfigModal(
-                                          (prevState) => !prevState
-                                      )
-                        }
-                        className="ml-2 btn btn-light btn-sm "
-                    >
-                        Save as default fontsize
-                    </button>
-                )}
-            </div>
-            <div className="">
-                <form className="charttitle-container form-inline">
-                    <label
-                        className="charttitle-label text-light font-weight-bold"
-                        hmtlfor="new-charttitle"
-                    >
-                        Charttitle
-                    </label>
-                    <input
-                        className="charttitle-input form-control form-control-sm"
-                        id="new-charttitle"
-                        onChange={handleChange}
-                        onKeyDown={handleKeyDown}
-                        placeholder={"Insert a new title"}
-                        ref={charttitleRef}
-                        type="text"
-                        value={chartTitle}
-                    />
-                </form>
-            </div>
-            {showsaveConfigModal && (
-                <SaveConfigModal
-                    config={{ fontSize }}
-                    setShowSaveConfigModal={setShowSaveConfigModal}
-                    show={showsaveConfigModal}
-                />
-            )}
-        </div>
-    );
-}
+	return (
+		<div className="bg-success mb-2 py-2 px-2 rounded-bottom">
+			<div className="fontsize-container d-flex flex-row flex-nowrap mb-2">
+				<span className="text-light font-weight-bold">Fontsize </span>
+				<Slider
+					min={10}
+					max={32}
+					startPoint={fontSize}
+					defaultValue={initialFontSize}
+					className="fontsize-slider py-2 ml-2"
+					onChange={handleRange}
+					railStyle={{
+						backgroundColor: "darkgreen",
+						height: "10px",
+						marginRight: "1em",
+					}}
+					handleStyle={{
+						backgroundColor: "#b9b9b9",
+						borderStyle: "solid",
+						borderWidth: "2px",
+						borderColor: "#606060",
+						padding: "8px",
+					}}
+					trackStyle={{
+						backgroundColor: "#313131",
+						height: "10px",
+					}}
+				/>
+				{/* ist nötig, weil lineChart.current.chartInstance.update() ein Error wirft, wenn die neue fontSize === aktuelle fontSize ist*/}
+				{fontSize !== initialFontSize && (
+					<button
+						onClick={
+							isAuthenticated
+								? handleSave
+								: () =>
+										setShowSaveConfigModal(
+											(prevState) => !prevState
+										)
+						}
+						className="ml-2 btn btn-light btn-sm "
+					>
+						Save as default fontsize
+					</button>
+				)}
+			</div>
+			<div className="">
+				<form className="charttitle-container form-inline">
+					<label
+						className="charttitle-label text-light font-weight-bold"
+						hmtlfor="new-charttitle"
+					>
+						Charttitle
+					</label>
+					<input
+						className="charttitle-input form-control form-control-sm"
+						id="new-charttitle"
+						onChange={handleChange}
+						onKeyDown={handleKeyDown}
+						placeholder={"Insert a new title"}
+						ref={charttitleRef}
+						type="text"
+						value={chartTitle}
+					/>
+				</form>
+			</div>
+			{showsaveConfigModal && (
+				<SaveConfigModal
+					config={{ fontSize }}
+					setShowSaveConfigModal={setShowSaveConfigModal}
+					show={showsaveConfigModal}
+				/>
+			)}
+		</div>
+	);
+};
 
 Options.propTypes = {
-    id: PropTypes.number.isRequired,
-    lineChart: PropTypes.object.isRequired,
-    options: PropTypes.object.isRequired,
+	id: PropTypes.number.isRequired,
+	lineChart: PropTypes.object.isRequired,
+	options: PropTypes.object.isRequired,
 };
 
 export default React.memo(Options);
--- client/src/components/ChartPage/ChartFetcher.js
+++ client/src/components/ChartPage/ChartFetcher.js
@@ -1,111 +1,104 @@
-import { defaults } from "react-chartjs-2"
 import PropTypes from "prop-types";
-import React, { useEffect, useRef } from 'react'
-import ChartWrapper from '../Browse/ChartWrapper'
-import { checkFetchedChartPageData, getConfig, getNewChartConfig } from "../../helper";
+import React, { useEffect, useRef } from "react";
+import ChartWrapper from "../Browse/ChartWrapper";
+import {
+	checkFetchedChartPageData,
+	getConfig,
+	getNewChartConfig,
+	setChartFontsize,
+} from "../../helper";
 import { useChartStore, useChartDispatch } from "../../stores/chartStore";
 
+const ChartFetcher = ({ chartData }) => {
+	const chartDispatch = useChartDispatch();
+	const { charts } = useChartStore();
 
-const ChartFetcher = ({chartData}) => {
-    
-    const chartDispatch = useChartDispatch();
-    const { charts } = useChartStore();
+	const initialRender = useRef(true);
 
-    const initialRender = useRef(true)
+	useEffect(() => {
+		const fetchData = async (chartData) => {
+			const {
+				chartType: configType,
+				customOptions: { fontSize },
+				data: {
+					colorIds,
+					customTableNames,
+					hours,
+					previousHour,
+					tableNames,
+				},
+				id,
+			} = chartData;
 
-    useEffect(() => {
-        const fetchData = async (chartData) => {
-            const {
-                chartType: configType,
-                customOptions: { fontSize },
-                data: {
-                    colorIds,
-                    customTableNames,
-                    hours,
-                    previousHour,
-                    tableNames,
-                },
-                id,
-            } = chartData;
-            chartDispatch({
-                type: "setCustomGlobalOptions",
-                payload: { fontSize },
-            });
-            defaults.global.defaultFontSize = fontSize;
-            defaults.global.legend.labels.defaultFontSize = fontSize;
+			const data = await Promise.all(
+				tableNames.map(async (query, _) => {
+					let [type, host, year, month, day, hour] = query.split("-");
+					hour = hour ? hour.split(":")[0] : "25";
+					const date = `${year}-${month}-${day}`;
+					const realFetch = async () => {
+						try {
+							const options = {
+								method: "GET",
+								headers: {
+									Accept: "application/json",
+								},
+							};
+							const response = await fetch(
+								`http://${process.env.REACT_APP_BACKEND_IP}:5000/api/data/categories/${type}?date=${date}&location=${host}&hour=${hour}`,
+								options
+							);
+							if (response.status === 404) {
+								alert(await response.json());
+								return null;
+							} else {
+								const data = await response.json();
+								return data;
+							}
+						} catch (e) {
+							console.log(e);
+						}
+					};
+					const fetchedData = await realFetch();
+					return fetchedData;
+				})
+			);
 
-            const data = await Promise.all(
-                tableNames.map(async (query, _) => {
-                    let [type, host, year, month, day, hour] = query.split("-");
-                    hour = hour ? hour.split(":")[0] : "25";
-                    const date = `${year}-${month}-${day}`;
-                    const realFetch = async () => {
-                        try {
-                            const options = {
-                                method: "GET",
-                                headers: {
-                                    Accept: "application/json",
-                                },
-                            };
-                            const response = await fetch(
-                                `http://${process.env.REACT_APP_BACKEND_IP}:5000/api/data/categories/${type}?date=${date}&location=${host}&hour=${hour}`,
-                                options
-                            );
-                            if (response.status === 404) {
-                                alert(await response.json());
-                                return null;
-                            } else {
-                                const data = await response.json();
-                                return data;
-                            }
-                        } catch (e) {
-                            console.log(e);
-                        }
-                    };
-                    const fetchedData = await realFetch();
-                    return fetchedData;
-                })
-            );
+			const config = { ...getConfig(configType), id };
 
-            const config = { ...getConfig(configType), id };
+			const checkedData = checkFetchedChartPageData(
+				data,
+				config,
+				configType,
+				previousHour,
+				hours,
+				tableNames,
+				customTableNames,
+				colorIds
+			);
 
-            const checkedData = checkFetchedChartPageData(
-                data,
-                config,
-                configType,
-                previousHour,
-                hours,
-                tableNames,
-                customTableNames,
-                colorIds
-            );
+			const newConfig = getNewChartConfig(checkedData, config, chartData);
+			const newChart = setChartFontsize(newConfig, fontSize);
+			charts.push(newChart);
+			chartDispatch({ type: "addNewChart", payload: charts });
+		};
 
-            const newConfig = getNewChartConfig(checkedData, config, chartData);
-            charts.splice(0, 1, newConfig);
-            chartDispatch({ type: "addNewChart", payload: charts });
-        };
+		if (initialRender.current) {
+			initialRender.current = false;
+		} else {
+			fetchData(chartData);
+		}
+		// eslint-disable-next-line
+	}, [chartData]);
 
-        if (initialRender.current) {
-            initialRender.current = false;
-        } else {
-            fetchData(chartData);
-        }
-        // eslint-disable-next-line
-    }, [chartData])
+	const Charts = charts.map((config, i) => {
+		return <ChartWrapper key={config.id} id={i} config={config} />;
+	});
 
-    const Charts = charts.map((config, i) => {
-        return <ChartWrapper key={config.id} id={i} config={config} />;
-    });
-
-    return (
-        <div>       
-            {Charts}
-        </div>
-    )
-}
-
-ChartFetcher.propTypes = {
-    chartData: PropTypes.object.isRequired,
+	return <div>{Charts}</div>;
 };
 
-export default ChartFetcher
+ChartFetcher.propTypes = {
+	chartData: PropTypes.object.isRequired,
+};
+
+export default ChartFetcher;
