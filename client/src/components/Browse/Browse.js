57ad79b2f8c84a066be57c4feb4a573f9810edf3
Max Schäfers <maximilian.schaefers@gmx.de>
2021-04-19 12:39:22 +0200

    DefaultOptions gets fetched only once after auth

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,18 +1,16 @@
 import { defaults } from "react-chartjs-2";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import AddChartButton from "./AddChartButton";
 import Charts from "./Charts";
 import { useAuthStore } from "../../stores/authStore";
 import { useChartStore, useChartDispatch } from "./../../stores/chartStore";
 
 const Browse = () => {
-	const { isAuthenticated, user, readyToRenderAfterAuth } = useAuthStore();
+	const { isAuthenticated, user } = useAuthStore();
 
 	const chartDispatch = useChartDispatch();
 
 	const { defaultOptions } = useChartStore();
-
-	const initialRender = useRef(true);
 
 	useEffect(() => {
 		chartDispatch({ type: "eraseCharts" });
@@ -20,46 +18,40 @@ const Browse = () => {
 	}, []);
 
 	useEffect(() => {
-		if (initialRender.current) {
-			initialRender.current = false;
-		}
-		else {
-			if (isAuthenticated && !defaultOptions.fontSize) {
-				const fetchConfig = async () => {
-					const { user_id } = user;
-					const options = {
-						credentials: "include",
-						method: "GET",
-						headers: {
-							Accept: "application/json",
-							"Content-Type": "application/json",
-						},
-					};
-					const response = await fetch(
-						`http://${process.env.REACT_APP_BACKEND_IP}:5000/api/user/${user_id}/config`,
-						options
-					);
-					if (!response.ok) {
-						defaultOptions.fontSize = 16;
-						defaults.global.defaultFontSize = 16;
-						defaults.global.legend.labels.defaultFontSize = 16;
-					} else {
-						const data = await response.json();
-						const { fontSize } = data;
-						defaultOptions.fontSize = fontSize;
-						defaults.global.defaultFontSize = fontSize;
-						defaults.global.legend.labels.defaultFontSize = fontSize;
-					}
+		if (isAuthenticated) {
+			const fetchDefaultOptions = async () => {
+				const { user_id } = user;
+				const options = {
+					credentials: "include",
+					method: "GET",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json",
+					},
 				};
-				fetchConfig();
-			} else {
-				defaultOptions.fontSize = 16;
-				defaults.global.defaultFontSize = 16;
-				defaults.global.legend.labels.defaultFontSize = 16;
-			}
+				const response = await fetch(
+					`http://${process.env.REACT_APP_BACKEND_IP}:5000/api/user/${user_id}/config`,
+					options
+				);
+				if (response.ok) {
+					const { fontSize } = await response.json();
+					defaultOptions.fontSize = fontSize;
+					defaults.global.defaultFontSize = fontSize;
+					defaults.global.legend.labels.defaultFontSize = fontSize;
+				} else {
+					defaultOptions.fontSize = 16;
+					defaults.global.defaultFontSize = 16;
+					defaults.global.legend.labels.defaultFontSize = 16;
+				}
+			};
+			fetchDefaultOptions();
+		} else {
+			defaultOptions.fontSize = 16;
+			defaults.global.defaultFontSize = 16;
+			defaults.global.legend.labels.defaultFontSize = 16;
 		}
 		// eslint-disable-next-line
-	}, [readyToRenderAfterAuth]);
+	}, [isAuthenticated]);
 
 	return (
 		<div>
